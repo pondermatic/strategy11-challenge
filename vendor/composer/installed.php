1d39875690b587d0f64658c1b0412cf164c96064
Pondermatic <git@pondermatic.com>
2024-04-17 11:18:47 -0500

    Add composer lock file and vendor directory.
    Does not include development packages.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pondermatic/strategy11-challenge',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '881c98a78de2049a38a080915887041e5a90700a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pondermatic/strategy11-challenge' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '881c98a78de2049a38a080915887041e5a90700a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
