0a5fe01c7be44aad07bc547360228e3237e68798
Pondermatic <git@pondermatic.com>
2024-04-17 07:34:55 -0500

    Set up Challenge_API in constructor.

--- includes/class-core.php
+++ includes/class-core.php
@@ -15,15 +15,26 @@ namespace Pondermatic\Strategy11\Challenge;
  */
 class Core {
 
+    const ROUTE_NAMESPACE = 'pondermatic-strategy11/v1';
+
     const VERSION = '1.0.0';
 
-	/**
+    /**
+     * An instance of the Challenge_API.
+     *
+     * @since 1.0.0
+     * @var Challenge_API
+     */
+    public static Challenge_API $challenge_api;
+
+    /**
 	 * Constructor.
 	 *
 	 * @since 1.0.0
 	 */
 	public function __construct() {
 		$this->add_hooks();
+        self::$challenge_api = new Challenge_API();
 	}
 
 	/**
