68f06e1bdbaa4238ebca43a1da5b1fc950c66ce9
Pondermatic <git@pondermatic.com>
2024-04-25 10:21:50 -0500

    Add bootstrap code for PHPUnit.

---
+++ tests/phpunit/bootstrap-loader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Loads and instantiates the PHPUnit bootstrap class for this plugin.
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+use Pondermatic\Strategy11\Challenge\PHPUnit\Bootstrap;
+
+require 'class-bootstrap.php';
+
+/* @noinspection PhpExpressionResultUnusedInspection */
+new Bootstrap();
---
+++ tests/phpunit/class-bootstrap.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Bootstrap class
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+namespace Pondermatic\Strategy11\Challenge\PHPUnit;
+
+use Pondermatic\WordpressPhpunitFramework\Bootstrap as FrameworkBootstrap;
+
+/**
+ * PHPUnit bootstrap.
+ *
+ * @since 1.0.0
+ */
+class Bootstrap extends FrameworkBootstrap {
+
+	/**
+	 * Main PHP File for the plugin.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	protected $plugin_main = 'pondermatic-strategy11-challenge.php';
+
+	/**
+	 * Install the plugin being tested and any plugins it is integrated with.
+	 *
+	 * @since 1.0.0
+	 */
+	public function install() {
+	}
+
+	/**
+	 * Manually load the plugin being tested and any dependencies.
+	 *
+	 * @since 1.0.0
+	 */
+	public function load(): void {
+
+		parent::load();
+	}
+}
